0a48c0b710915cc87f326c8aef4ac40cdd2e1581
Patricia <pgutierrezrivas@gmx.es>
2024-10-31 20:18:28 +0100

    load product table and shopping cart

--- static/js/carrito.js
+++ static/js/carrito.js
@@ -4,14 +4,20 @@ export default class Carrito {
         this.productosCarrito = [];
     }
 
-    actualizarUnidades(nuevoProducto, nuevaCantidad) { // Actualiza el número de unidades que se quieren comprar de un producto
+    // Busca en el carrito un producto por su sku (creo este metodo para no tener que repetir codigo en 'actualizarUnidades' y 'obtenerInformacionProducto')
+    buscarProductoPorSku(sku) {
+        /*return this.productosCarrito.find(function(producto) { (uso arrow functions (podria simplificar aun mas al tener una sola instruccion pero por entenderlo yo mejor lo dejo asi))*/
+        return this.productosCarrito.find((producto) => { 
+            return producto.sku === sku;
+        });
+    }
+
+    // Actualiza el número de unidades que se quieren comprar de un producto
+    actualizarUnidades(nuevoProducto, nuevaCantidad) {
 
         nuevoProducto.quantity = nuevaCantidad; // asigno el numero de unidades que llegan al atributo quantity del objeto nuevoProducto
-        
-        /*const productoEnCarrito = this.productosCarrito.find(function(productoActual) { (uso arrow functions (podria simplificar aun mas al tener una sola instruccion pero por entenderlo yo mejor lo dejo asi))*/
-        const productoEnCarrito = this.productosCarrito.find((productoActual) => {
-            return productoActual.sku === nuevoProducto.sku;
-        }); // busco en el carrito si ya existe un producto con el mismo sku
+
+        const productoEnCarrito = this.buscarProductoPorSku(nuevoProducto.sku); // busco por sku
 
         if (productoEnCarrito) { 
             productoEnCarrito.quantity = nuevaCantidad; // si el producto existe, actualizo su cantidad con la nueva que llega
@@ -21,13 +27,12 @@ export default class Carrito {
         }
     }
 
-    obtenerInformacionProducto(sku) { // Devuelve los datos de un producto además de las unidades seleccionadas
+    // Devuelve los datos de un producto además de las unidades seleccionadas
+    obtenerInformacionProducto(sku) {
 
-        const productoEncontrado = this.productosCarrito.find((productoActual) => {
-            return productoActual.sku === sku;
-        });
+        const productoEncontrado = this.buscarProductoPorSku(sku); // busco por sku
 
-        if (productoEncontrado) { // si el producto se encuentra
+        if (productoEncontrado) { // si encuentra el producto
             return { // devuelvo un objeto con su informacion detallada 
                 sku: productoEncontrado.sku,
                 title: productoEncontrado.title,
@@ -41,20 +46,21 @@ export default class Carrito {
         }
     }
 
-    obtenerCarrito() { // Devuelve información de los productos añadidos al carrito // Además del total calculado de todos los productos
+    // Devuelve información de los productos añadidos al carrito
+    // Además del total calculado de todos los productos
+    obtenerCarrito() {
 
-        const desgloseProductosEnCarrito = this.productosCarrito.map((producto) => { // aplico metodo 'map' al array que itera sobre cada producto añadido al carrito (uso map ya que quiero hacer algo con todos los elementos del array (pero no entre ellos))
+        const productosAñadidos = this.productosCarrito.map((producto) => { // aplico metodo 'map' al array que itera sobre cada producto añadido al carrito (uso map ya que quiero hacer algo con todos los elementos del array (pero no entre ellos))
             return this.obtenerInformacionProducto(producto.sku); // uso metodo 'obtenerInformacionProducto' (de mi clase Carrito) para poder convertir cada producto del array en un objeto y obtener asi toda su informacion detallada
         });
 
-
-        const totalCarrito = desgloseProductosEnCarrito.reduce((totalAcumulado, producto) => { // aplico metodo 'reduce' para poder hacer una operacion con todos los elementos del array (entre ellos)
-                return totalAcumulado + producto.total; // calculo el total de todos los productos en el carrito (por cada producto, se suma su propiedad total al totalAcumulado)
+        const sumaTotal = productosAñadidos.reduce((totalAcumulado, producto) => { // aplico metodo 'reduce' para poder hacer una operacion con todos los elementos del array (entre ellos)
+                return totalAcumulado + producto.total; // calculo el total de todos los productos en el carrito (por cada producto, se suma su propiedad 'total' al totalAcumulado)
             }, 0); // 0 es el valor inical del parametro acumulador
 
         return {
-            products: desgloseProductosEnCarrito,
-            total: totalCarrito.toFixed(2) // redondeo el total a dos decimales
+            productosEnCarrito: productosAñadidos,
+            totalCarrito: Number(sumaTotal.toFixed(2)) // redondeo el total a dos decimales
         };
     }
 }
--- static/js/script.js
+++ static/js/script.js
@@ -7,9 +7,128 @@ var productos = []; // creo mi variable productos
 document.addEventListener('DOMContentLoaded', function (event) {
 
     function cargarTabla(productos) { // creo funcion para rellenar la tabla con los datos que tenemos en el json
+        
+        const tablaProductos = document.getElementById("tablaProductos"); // selecciono la tabla de productos mediante su id
+        
+        productos.forEach((producto) => { // itero sobre el array para crear filas en la tabla (por cada producto creo una fila)
+            
+            const filaProducto = document.createElement('tr'); // creo una fila para cada producto
+            
+            const miproducto = { //creo un objeto 'miproducto' para poder trabajar con toda la informacion del producto
+                sku: producto.SKU,
+                title: producto.title,
+                price: Number(producto.price)
+            }
+
+            // PRIMERA CELDA: descripcion del producto (titulo y sku)
+            const celdaDescripcion = document.createElement('td'); // creo una celda
+
+            const nombreProducto = document.createElement('p'); // creo un parrafo para el nombre
+            nombreProducto.innerText = miproducto.title; // relleno contenido del parrafo con la propiedad 'title' del objeto 'miproducto'
+            nombreProducto.classList.add('bold'); // aplico estilos css
+
+            const skuProducto = document.createElement('p'); // creo un parrafo para el sku
+            skuProducto.innerText = "REF: " + miproducto.sku; // relleno contenido del parrafo con la propiedad 'sku' del objeto 'miproducto'
+
+            celdaDescripcion.append(nombreProducto, skuProducto); // añado los dos parrafos a la celda
+            filaProducto.appendChild(celdaDescripcion); // añado la celda a la fila
+
+            // SEGUNDA CELDA: cantidad del producto (botones e input)
+            const celdaCantidad = document.createElement('td'); // creo celda para la cantidad
+
+            // boton quitar producto
+            const btnDisminuir = document.createElement('button'); // creo boton
+            btnDisminuir.innerText = '-'; // añado texto al boton
+            btnDisminuir.classList.add('btnNum'); // aplico estilos css
+
+            // input valor producto
+            const input = document.createElement('input'); // creo input para mostrar la cantidad
+            input.type = 'text'; // tipo del input texto
+            input.value = '0'; // inicializo el input a cero
+            input.readOnly = true; // hago que no se pueda modificar manualmente (solo se modifica mediante los botones)
+            input.classList.add('inputNum'); // aplico estilos css
+
+            // boton añadir producto
+            const btnAumentar = document.createElement('button'); // misma mecanica que con el boton quitar
+            btnAumentar.innerText = '+';
+            btnAumentar.classList.add('btnNum');
+
+            // funcion para actualizar el importe total por producto al interactuar con los botones (y por tanto, cambiar el valor de la cantidad)
+            function actualizarTotalPorProducto(price, quantity) {
+                const totalProducto = price * quantity;
+                celdaTotal.innerText = totalProducto.toFixed(2) + " " + moneda; // muestro total redondeado a 2 decimales
+            };
+
+            // funcion para actualizar la cantidad
+            function actualizarCantidad(incremento) { 
+                let valorActual = parseInt(input.value); // valor actual del input como numero
+                let nuevoValor = valorActual + parseInt(incremento); // calculo el nuevo valor (parseo incremento para que nuevoValor sea tipo number)
+
+                if (nuevoValor >= 0) { // verifico que el valor no sea negativo
+                    input.value = nuevoValor; // actualizo el valor del input
+                    actualizarTotalPorProducto(miproducto.price, nuevoValor); // actualizo el total    
+                    miCarrito.actualizarUnidades(miproducto, nuevoValor); // actualizo las unidades en el carrito
+                    cargarCarrito(); // actualizo el estado global del carrito
+                }
+            }
+
+            // agrego eventos a los botones
+            btnDisminuir.addEventListener('click', function (event) {
+                actualizarCantidad(-1); // disminuyo la cantidad
+            });
+
+            btnAumentar.addEventListener('click', function (event) {
+                actualizarCantidad(1); // aumento la cantidad
+            });
+
+            celdaCantidad.append(btnDisminuir, input, btnAumentar); // añado los botones y el input a la celda de cantidad
+            filaProducto.appendChild(celdaCantidad);
+
+            // TERCERA CELDA: precio unitario del producto
+            const celdaPrecio = document.createElement('td');
+            celdaPrecio.innerText = miproducto.price + " " + moneda; // relleno contenido del parrafo con la propiedad 'price' del objeto 'miproducto' + mi variable moneda
+            filaProducto.appendChild(celdaPrecio);
+
+            // CUARTA CELDA: total para ese producto
+            const celdaTotal = document.createElement('td');
+            celdaTotal.innerText = "0.00 " + moneda; // inicializo a cero el total (ira cambiando en funcion del valor que tenga el input)
+            filaProducto.appendChild(celdaTotal);
+
+            // POR ULTIMO, añado la fila a la tabla de productos
+            tablaProductos.appendChild(filaProducto);
+        });
+
     }
 
     function cargarCarrito() { // creo funcion para rellenar el carrito con los productos que se van añadiendo a la cesta 
+        
+        const contenedorCarrito = document.getElementById('desgloseCarrito');
+        contenedorCarrito.innerHTML = '';  // limpio contenido previo
+        
+        const desgloseCarrito = miCarrito.obtenerCarrito(); // llamo al metodo 'obtenerCarrito' de la clase Carrito para que me devuelva los productos añadidos y el total acumulado
+
+        desgloseCarrito.productosEnCarrito.forEach((productoCarrito) => {
+
+            if (productoCarrito.quantity > 0) { // si el producto tiene mas de 0 unidades, añado fila
+
+                const filaProductoCarrito = document.createElement('tr'); // creo una fila por cada producto añadido al carrito
+                filaProductoCarrito.classList.add('justifySpace'); // aplico estilos css
+
+                const celdaCantidad = document.createElement('td'); // creo celda
+                celdaCantidad.innerText = productoCarrito.quantity + " x " + productoCarrito.title; // relleno la celda
+
+                const celdaTotal = document.createElement('td'); // creo celda
+                celdaTotal.innerText = (productoCarrito.price * productoCarrito.quantity).toFixed(2) + " " + moneda; // relleno la celda
+
+                filaProductoCarrito.append(celdaCantidad, celdaTotal); // añado las celdas a la fila
+
+                contenedorCarrito.appendChild(filaProductoCarrito); // añado la fila al contenedor
+            }
+        });
+
+        const totalFinal = document.getElementById('totalFinal');
+        totalFinal.innerText = desgloseCarrito.totalCarrito + " " + moneda; // muestro el total final
+
     }
 
     fetch('https://jsonblob.com/api/1300890895002755072') // uso el fetch para poder obtener el json con los productos y mostrarlos en la tabla
